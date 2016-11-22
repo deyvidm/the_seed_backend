b742155080dfc2038f62583d75b9b6ef84f04be6
Deyvid <dmitkov@mail.uoguelph.ca>
2016-11-22 14:58:24 -0500

    updated database structure

--- db_repository/versions/002_migration.py
+++ db_repository/versions/002_migration.py
@@ -5,15 +5,25 @@ from migrate import *
 from migrate.changeset import schema
 pre_meta = MetaData()
 post_meta = MetaData()
+entry = Table('entry', post_meta,
+    Column('id', Integer, primary_key=True, nullable=False),
+    Column('product_id', Integer),
+    Column('location_id', Integer),
+    Column('name', String(length=100)),
+    Column('date', DateTime),
+)
+
 
 def upgrade(migrate_engine):
     # Upgrade operations go here. Don't create your own engine; bind
     # migrate_engine to your metadata
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
+    post_meta.tables['entry'].create()
 
 
 def downgrade(migrate_engine):
     # Operations to reverse the above upgrade go here.
     pre_meta.bind = migrate_engine
     post_meta.bind = migrate_engine
+    post_meta.tables['entry'].drop()
--- src/models.py
+++ src/models.py
@@ -1,20 +1,25 @@
+#!flas/bin/python
 from src import db
 from sqlalchemy.sql import func
 
 class Product(db.Model):
+    __tablename__ = 'product'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True, unique=True)
+    entries = db.relationship('Entry', backref='food', lazy='dynamic')
 
 class Location(db.Model): 
+    __tablename__ = 'location'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True, unique=True)
 
-class Entries(db.Model):
+class Entry(db.Model):
+    __tablename__ = 'entry'
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey("Product.id"))
-    location_id = db.Column(db.Integer, db.ForeignKey("Location.id"))
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'))
     # Decimal(10,2) is a number with 8 digits before the decimal and 2 digts after
     price = db.column(db.Float(10,2))
     name = db.Column(db.String(100), index=True, unique=True)
-    date = db.Column(db.DateTime, default=func.now())
+    date = db.Column(db.DateTime)
 
