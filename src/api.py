0275f7d995896111534a64c7f5d1e1ff131ddae5
Deyvid <dmitkov@mail.uoguelph.ca>
2016-11-22 15:32:07 -0500

    enhanced balls

--- src/api.py
+++ src/api.py
@@ -3,4 +3,15 @@ from src import app
 @app.route("/")
 @app.route("/index")
 def index():
-    return "balls"
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+
+  <body>
+    balls
+    <img src="http://i284.photobucket.com/albums/ll36/Bigsteve87/Gifs/CleaningBowlingBalls.gif">
+  </body>
+</html>
+''' 
